a4028abde540e0883acfd61a8729cd0073bacfee
dbarrous <damianbarrous@gmail.com>
2019-04-13 22:51:58 -0400

    Merge branch 'master' of /Users/damianbarrous/WebstormProjects/volunteer-connect with conflicts.

--- src/subcomponents/User/UserImage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class UserImage extends React.Component {
-  render() {
-    return <img src={this.props.img} alt={"user"} />;
-  }
-}
--- src/subcomponents/User/UserName.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserName(props) {
-  return <h1 className={"userName"}>{props.name}</h1>;
-}
